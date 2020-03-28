0fc7eebb6b40d2780f0e4dfcbaa3634d35d6e517
Dosljak Velibor <dosljakvelibor@gmail.com>
2020-03-28 09:42:28 -0400

    =test

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express()
 app.use(express.static(__dirname));
 const port = process.env.PORT || 3000
 
-app.get('*', (req, res) => res.send("test")))//res.sendFile(__dirname+'/html.html'))
+app.get('*', (req, res) => res.send("test"))//res.sendFile(__dirname+'/html.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
